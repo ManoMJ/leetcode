d3b8902a4ff513f88be9eb0548ad13f554cee287
ManoMJ <46708194+ManoMJ@users.noreply.github.com>
2024-12-08 20:18:00 +0900

    Time: 212 ms (21.15%), Space: 26.7 MB (32.38%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,14 +1,16 @@
-class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        answer = [0] * len(temperatures)
-        stack = []
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        st = []
+        results = [0] * len(temperatures)
+        
         for i in range(len(temperatures)-1, -1, -1):
-            cnt = 0
-            while stack and temperatures[stack[-1]] <= temperatures[i]:
-                stack.pop()
+            while st and temperatures[st[-1]] <= temperatures[i]:
+                st.pop()
+            if st and temperatures[st[-1]] > temperatures[i]:
+                results[i] = st[-1] - i
+            st.append(i)
             
-            if stack:
-                answer[i] = stack[-1] - i
+        
+        return results
+        
 
-            stack.append(i)
-        return answer
