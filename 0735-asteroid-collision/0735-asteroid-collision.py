ec608e64dfb563f60e4aa09ee39e109da611ea18
ManoMJ <46708194+ManoMJ@users.noreply.github.com>
2025-02-20 21:21:16 +0900

    Time: 3 ms (94.22%), Space: 18.8 MB (86.16%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -26,17 +26,5 @@ class Solution:
                     stack.append(a1)
             else:
                 stack.append(a1)
-        
-        print("stack", stack)
-
-        while stack and len(stack)>1 and stack[-2] > 0 and stack[-1] < 0:
-            
-            a1 = stack.pop()
-            a2 = stack.pop()
-            print(a1, a2)
-            if abs(a1) > abs(a2):
-                stack.append(a1)
-            elif abs(a1) < abs(a2) :
-                stack.append(a2)
                 
         return stack
