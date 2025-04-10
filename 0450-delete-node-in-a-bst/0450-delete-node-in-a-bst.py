08a549dcd7233b6317236b748e9d895b5a61e10b
ManoMJ <46708194+ManoMJ@users.noreply.github.com>
2025-04-10 21:45:21 +0900

    Time: 1 ms (52.33%), Space: 20.5 MB (35.21%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -1,58 +1,32 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution(object):
     def deleteNode(self, root, key):
-        node = root
-        parent = None
-
-        while node:
-            if node.val==key:
-                break
-            elif node.val > key:
-                parent = node
-                node = node.left
-            else:
-                parent = node
-                node = node.right
-
-        if not node:
-            return root
+        if not root:
+            return None
         
-        # 1 : 말단 노드
-        if not node.left and not node.right:
-            if not parent:
-                return None
-            elif parent.val > node.val:
-                parent.left = None
-            else:
-                parent.right = None
-        
-        # 2 : 왼쪽 자식만 있는 경우
-        elif node.left and not node.right:
-            if not parent:
-                root = node.left
-            elif parent.val > node.val:
-                parent.left = node.left
-            else:
-                parent.right = node.left
-        # 3 : 오른쪽 자식이 있는 경우
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
         else:
-            smaller = node.right
-            smallerParent = node
-            while smaller.left:
-                smallerParent = smaller
-                smaller = smaller.left
-            if smallerParent.val > smaller.val:
-                smallerParent.left = smaller.right
+            # Case 1: No child
+            if not root.left and not root.right:
+                return None
+            # Case 2: One child
+            elif not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            # Case 3: Two children
             else:
-                smallerParent.right = smaller.right
-            node.val = smaller.val
-            
+                # Find in-order successor (smallest in right subtree)
+                successor = self.findMin(root.right)
+                root.val = successor.val
+                # Delete the successor recursively
+                root.right = self.deleteNode(root.right, successor.val)
         
         return root
-            
-        
+
+    def findMin(self, node):
+        while node.left:
+            node = node.left
+        return node
