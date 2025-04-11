7b68e5d77e3af8c911093eb8e674c39403e2f7e3
ManoMJ <46708194+ManoMJ@users.noreply.github.com>
2025-04-11 22:01:35 +0900

    Time: 8 ms (88.97%), Space: 18.4 MB (8.3%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -2,49 +2,24 @@ from collections import deque
 
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
-        q = deque()
         n = len(senate)
-    
-        radient_cnt = senate.count("R")
-        dire_cnt = senate.count("D")
-        senate = deque(map(str, senate))
-        radient_pointer, dire_pointer = 0, 0
+        radiant = deque()
+        dire = deque()
 
-        pointer = 0
+        for i in range(n):
+            if senate[i] == 'D':
+                dire.append(i)
+            else:
+                radiant.append(i)
         
-        print("radient : ", radient_cnt, "dire : ", dire_cnt)
-
-        while True:
+        while radiant and dire:
            
-
-            if radient_cnt == 0:
-                return "Dire"
-            elif dire_cnt == 0:
-                return "Radiant"
-            
-            if senate[pointer] == 'R':
-                radient_pointer = pointer
-                dire_pointer = pointer
-                while True:
-                    if senate[dire_pointer] == 'D':
-                        break
-                    dire_pointer = (dire_pointer+1)%n
-                if dire_pointer < n:
-                    senate[dire_pointer] = 'Q'
-                    dire_cnt -= 1
-            elif senate[pointer] == 'D':
-                dire_pointer = pointer
-                radient_pointer = pointer
-                while radient_pointer < n:
-                    if senate[radient_pointer] == 'R':
-                        break
-                    radient_pointer = (radient_pointer+1)%n
-                if radient_pointer < n:
-                    senate[radient_pointer] = 'Q'
-                    radient_cnt -= 1
-            
-            pointer = (pointer+1)%n
-
+            r = radiant.popleft()
+            d = dire.popleft()
+            if r < d:
+                radiant.append(n)
+            else:
+                dire.append(n)
+            n += 1
         
-            
-        
+        return "Radiant" if radiant else "Dire"
