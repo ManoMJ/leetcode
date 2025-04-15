3422c8af0f60c66993e2e5ab30b7acb0a6e34050
ManoMJ <46708194+ManoMJ@users.noreply.github.com>
2025-04-15 15:52:53 +0900

    Time: 0 ms (100%), Space: 14.1 MB (51.31%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -2,16 +2,9 @@ from collections import defaultdict
 
 class Solution(object):
     def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        numbers_dict = defaultdict(int)
-
+        unique = 0
         for num in nums:
-            numbers_dict[num] += 1
-    
-        for num in numbers_dict.keys():
-            if numbers_dict[num] < 2:
-                return num
+            unique ^= num
         
+        return unique
+    
