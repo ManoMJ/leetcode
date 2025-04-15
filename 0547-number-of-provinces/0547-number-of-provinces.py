b33de0b322827bb07ce3b9331e00a609d4614c2f
ManoMJ <46708194+ManoMJ@users.noreply.github.com>
2025-04-15 16:35:26 +0900

    Time: 0 ms (100%), Space: 13.8 MB (8.77%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -4,17 +4,20 @@ class Solution(object):
         :type isConnected: List[List[int]]
         :rtype: int
         """
-        result = 0
+        def dfs(city):
+            for neighbor in range(n):
+                if isConnected[city][neighbor]==1 and neighbor not in visited:
+                    visited.add(neighbor)
+                    dfs(neighbor)
+
+        n = len(isConnected)
         visited = set()
-        for i in range(len(isConnected)):
-            stack = [i]
-            if i not in visited:
-                result += 1
-                while stack:
-                    top = stack.pop()
-                    for j, link in enumerate(isConnected[top]):
-                        if link==1 and j not in visited:
-                            visited.add(j)
-                            stack.append(j)
-                
-        return result
+        provinces = 0
+
+        for city in range(n):
+            if city not in visited:
+                visited.add(city)
+                dfs(city)
+                provinces += 1
+
+        return provinces
