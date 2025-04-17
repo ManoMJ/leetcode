aecc3122bbc6a53215096e01d32150f638c0c337
ManoMJ <46708194+ManoMJ@users.noreply.github.com>
2025-04-17 20:57:33 +0900

    Time: 343 ms (100%), Space: 14.7 MB (11.11%) - LeetHub

--- 2422-query-kth-smallest-trimmed-number/2422-query-kth-smallest-trimmed-number.py
+++ 2422-query-kth-smallest-trimmed-number/2422-query-kth-smallest-trimmed-number.py
@@ -1,25 +1,22 @@
 from collections import defaultdict
-
+# 1318 ms
 class Solution(object):
     def smallestTrimmedNumbers(self, nums, queries):
-        """
-        :type nums: List[str]
-        :type queries: List[List[int]]
-        :rtype: List[int]
-        """
-        output = []
-        n = len(nums)
-        num_length = len(nums[0])
-        trimmed = defaultdict(list)
-        for i, num in enumerate(nums):
-            for j in range(1, num_length+1): 
-                trimmed[j].append( ( int(num[-j:]), i) )
+        # Collect Only the needed trim lengths from the queries
+        needed_trims = {trim for _, trim in queries}
 
-        for k in trimmed.keys():
-            trimmed[k].sort(key= lambda x : x[0])
+        # Precompute a dictionary for these trim lengths using lexicographic order.
+        trimmed = {}
+        for t in needed_trims:
+            # Create a list of (trimmed_value, original_index) tuples.
+            # Since all trimmed strings are of equal length, lexicographic order works.
+            trimmed[t] = sorted((num[-t:], i) for i, num in enumerate(nums))
 
-        for k, trim_length in queries:
-            output.append(trimmed[trim_length][k-1][1])    
-        return output
+        # For each query, find the k-th smallest trimed number's original index.
+        result = []
+        for k, t in queries:
+            result.append(trimmed[t][k-1][1])
+
+        return result
         
         
