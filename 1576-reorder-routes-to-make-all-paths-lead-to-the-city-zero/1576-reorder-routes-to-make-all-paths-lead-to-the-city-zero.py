c741739ba06e833ce67681e193e63a1bb63890c0
ManoMJ <46708194+ManoMJ@users.noreply.github.com>
2025-04-17 17:56:34 +0900

    Time: 172 ms (78.46%), Space: 50.3 MB (54.06%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -2,29 +2,25 @@ from collections import defaultdict
 
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        self.result = 0
-
-        def dfs(current, visited):
-             for road in graph[current]:
-                if road not in visited:
-                    if (current, road) in one_way:
-                        self.result += 1
-                    visited.add(road)
-                    dfs(road, visited)
-
-        one_way = set()
-        graph = defaultdict(list)
-        for x, y in connections:
-            graph[x].append(y)
-            graph[y].append(x)
-            one_way.add( (x, y) )
-      
-
-        for road in graph[0]: 
-            visited = {0, road}
-            if (0, road) in one_way:
-                self.result += 1
-            dfs(road, visited)
-
-        return self.result
+        # Store directed roads for fast lookup
+        directed = set((a, b) for a, b in connections)
         
+        # Undirected graph representation
+        graph = defaultdict(list)
+        for a, b in connections:
+            graph[a].append(b)
+            graph[b].append(a)
+
+        def dfs(node, parent):
+            nonlocal count
+            for neighbor in graph[node]:
+                if neighbor == parent:
+                    continue
+                # If the direction is away from node 0, we need to reverse it
+                if (node, neighbor) in directed:
+                    count += 1
+                dfs(neighbor, node)
+
+        count = 0
+        dfs(0, -1)
+        return count
