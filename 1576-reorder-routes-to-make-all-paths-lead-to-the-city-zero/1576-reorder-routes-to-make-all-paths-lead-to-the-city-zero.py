5bc14d0e06e50c4b59d99ea9ebd8714b3e997c2d
ManoMJ <46708194+ManoMJ@users.noreply.github.com>
2025-04-17 18:06:14 +0900

    Time: 164 ms (83.43%), Space: 50.1 MB (54.47%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -15,9 +15,12 @@ class Solution:
             nonlocal count
             for neighbor in graph[node]:
                 if neighbor == parent:
-                    if (node, neighbor) in directed:
-                        count += 1
-                    dfs(neighbor, node)
+                    continue
+                
+                # If the direction is away from node 0, we need to reverse it
+                if (node, neighbor) in directed:
+                    count += 1
+                dfs(neighbor, node)
 
         count = 0
         dfs(0, -1)
