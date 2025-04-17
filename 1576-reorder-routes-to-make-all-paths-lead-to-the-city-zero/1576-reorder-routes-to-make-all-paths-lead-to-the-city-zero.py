ca369ca58fab4141314f040be93914ec63d88164
ManoMJ <46708194+ManoMJ@users.noreply.github.com>
2025-04-17 17:54:14 +0900

    Time: 238 ms (22.63%), Space: 53.3 MB (19.51%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -7,7 +7,7 @@ class Solution:
         def dfs(current, visited):
              for road in graph[current]:
                 if road not in visited:
-                    if road in one_way[current]:
+                    if current not in one_way[road]:
                         self.result += 1
                     visited.add(road)
                     dfs(road, visited)
@@ -22,7 +22,7 @@ class Solution:
 
         for road in graph[0]: 
             visited = {0, road}
-            if [road, 0] not in connections:
+            if 0 not in one_way[road]:
                 self.result += 1
             dfs(road, visited)
 
